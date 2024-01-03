e6aeea0817c95fce108daa3887f72791178b484e
Diogo Ribeiro <pdcribeiro95@gmail.com>
2024-01-03 19:22:06 +0000

    Add back navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from 'next/headers';
 
+import AuthButton from '@/components/AuthButton';
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Page() {
@@ -18,5 +19,36 @@ export default async function Page() {
     .select()
     .eq('owner', user.id);
 
-  return <pre>{JSON.stringify(elements, null, 2)}</pre>;
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <div></div>
+          <AuthButton />
+        </div>
+      </nav>
+
+      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+        {/* <Header /> */}
+        <main className="flex-1 flex flex-col gap-6">
+          {/* <h2 className="font-bold text-4xl mb-4">Next steps</h2> */}
+          <pre>{JSON.stringify(elements, null, 2)}</pre>
+        </main>
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{' '}
+          <a
+            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            Supabase
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
